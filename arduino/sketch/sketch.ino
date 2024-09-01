5231a530bc679fa5e193833f82ce1b79f82e5804
Antoine Fricker <antoine@marmelab.com>
2024-09-01 18:34:23 +0200

    feat: read serial to assign led

--- arduino/sketch/sketch.ino
+++ arduino/sketch/sketch.ino
@@ -2,24 +2,30 @@ int buzzer = 2;
 int ledPins[] = {13, 12, 11, 10, 9, 8, 7, 6, 5, 4};
 int ledsNumber = sizeof(ledPins)/sizeof(int);
 
-bool toggler = true;
+int value = 0;
 
 void setup() {
+  Serial.begin(9600);
+   Serial.setTimeout(10);
+  
   for (int i = 0; i < ledsNumber; i++) {
     pinMode(ledPins[i], OUTPUT);
   }
 
   pinMode(buzzer, OUTPUT);
-}
+} 
 
 void loop() {
-  toggler = !toggler;
+  if (Serial.available() > 0) {
+    String str = Serial.readString();
+    str.trim();
+    
+    value = str.toInt();
+    Serial.println(value);
+    
+  }
 
   for (int i = 0; i < ledsNumber; i++) {
-    digitalWrite(ledPins[i], toggler ? LOW : HIGH);
-    tone(buzzer, 100 + 10 * i);
-    delay(50);
+      digitalWrite(ledPins[i], i >= value ? LOW : HIGH);
   }
-  noTone(buzzer);
-  delay(1500);
 }
